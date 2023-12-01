0fbc6625be3b325868fa5fbdae17870ba62d5b52
Matt Harden <matt.harden@gmail.com>
2023-12-01 15:21:41 -0800

    Remove the boilerplate test from day1 main.zig.

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -109,10 +109,3 @@ pub fn main() !void {
     try part1();
     try part2();
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
