e000d9d48127611c7ce02598a72fa52e0f6f04ac
Matt Harden <matt.harden@gmail.com>
2023-12-02 12:03:56 -0800

    More rewriting of day 1.

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -52,46 +52,27 @@ fn part1(input: []const u8) !void {
     std.debug.print("{d}\n", .{sum});
 }
 
-const WrittenDigit = struct {
-    text: []const u8,
-    value: u8,
-    fn match(self: WrittenDigit, buffer: []const u8) ?u8 {
-        if (std.mem.startsWith(u8, buffer, self.text)) {
-            return self.value;
-        }
-        return null;
-    }
-};
-
-const WrittenDigits = [_]WrittenDigit{
-    .{ .text = "one", .value = 1 },
-    .{ .text = "two", .value = 2 },
-    .{ .text = "three", .value = 3 },
-    .{ .text = "four", .value = 4 },
-    .{ .text = "five", .value = 5 },
-    .{ .text = "six", .value = 6 },
-    .{ .text = "seven", .value = 7 },
-    .{ .text = "eight", .value = 8 },
-    .{ .text = "nine", .value = 9 },
-    .{ .text = "0", .value = 0 },
-    .{ .text = "1", .value = 1 },
-    .{ .text = "2", .value = 2 },
-    .{ .text = "3", .value = 3 },
-    .{ .text = "4", .value = 4 },
-    .{ .text = "5", .value = 5 },
-    .{ .text = "6", .value = 6 },
-    .{ .text = "7", .value = 7 },
-    .{ .text = "8", .value = 8 },
-    .{ .text = "9", .value = 9 },
-};
-
-fn matchWrittenDigit(buffer: []const u8) ?u8 {
-    for (WrittenDigits) |writtenDigit| {
-        if (writtenDigit.match(buffer)) |value| {
-            return value;
-        }
+fn matchString(text: []const u8, value: u8, buffer: []const u8) ?u8 {
+    if (std.mem.startsWith(u8, buffer, text)) {
+        return value;
     }
     return null;
+}
+
+fn matchWrittenDigit(buffer: []const u8) ?u8 {
+    return matchString("one", 1, buffer) orelse
+        matchString("two", 2, buffer) orelse
+        matchString("three", 3, buffer) orelse
+        matchString("four", 4, buffer) orelse
+        matchString("five", 5, buffer) orelse
+        matchString("six", 6, buffer) orelse
+        matchString("seven", 7, buffer) orelse
+        matchString("eight", 8, buffer) orelse
+        matchString("nine", 9, buffer);
+}
+
+fn matchAnyDigit(buffer: []const u8) ?u8 {
+    return matchDigit(buffer) orelse matchWrittenDigit(buffer);
 }
 
 test "firstMatch, matchWrittenDigit" {
@@ -110,7 +91,7 @@ fn part2(input: []const u8) !void {
     var linesIter = std.mem.splitScalar(u8, input, '\n');
     var sum: u16 = 0;
     while (linesIter.next()) |line| {
-        sum += digits(line, matchWrittenDigit) orelse return error.NoDigits;
+        sum += digits(line, matchAnyDigit) orelse return error.NoDigits;
     }
     std.debug.print("{d}\n", .{sum});
 }
